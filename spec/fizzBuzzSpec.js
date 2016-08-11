f11babd4ceb5fd601825c12deb5f2cba18f69e84
Sara <sarah.alalaya@gmail.com>
2016-08-11 12:14:20 +0300

    (Redoing the challenge): (isDevisibleByFifteen is fully tested)

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -13,14 +13,20 @@ describe('Fizzbuzz',function(){
     it('5',function(){
       expect(game.isDivisibleByFive(5)).toBe(true);
     });
+    it('15',function(){
+      expect(game.isDivisibleByFiftenn(15)).toBe(true);
+    });
   });
 
   describe('Knows when a number is not divisible by', function(){
     it('3', function(){
       expect(game.isDivisibleByThree(7)).toBe(false);
     });
-    it('5', function(){
+    it('5',function(){
       expect(game.isDivisibleByFive(7)).toBe(false);
+    });
+    it('15',function(){
+      expect(game.isDivisibleByFiftenn(7)).toBe(false);
     });
   });
 });
